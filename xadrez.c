1469788fe8bd9de73a54228b0288dcbd14c6d517
Aleex Brendon <alexbrendo.dev@gmail.com>
2026-02-15 02:54:37 -0300

    Tema 4: Nivel Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
     int casasTorre = 5;
     int casasBispo = 5;
     int casasRainha = 8;
-
+    int casasCavaloVertical = 2;
+    int casasCavaloHorizontal = 1;
     int i;
     int contador;
 
     printf("Movimento da Torre:\n");
 
-    for(i = 0; i < casasTorre; i++) {
+    for (i = 0; i < casasTorre; i++)
+    {
         printf("Direita\n");
     }
 
@@ -19,7 +22,8 @@ int main() {
 
     contador = 0;
 
-    while(contador < casasBispo) {
+    while (contador < casasBispo)
+    {
         printf("Cima Direita\n");
         contador++;
     }
@@ -28,11 +32,25 @@ int main() {
 
     contador = 0;
 
-    do {
+    do
+    {
         printf("Esquerda\n");
         contador++;
-    } while(contador < casasRainha);
+    } while (contador < casasRainha);
 
+    printf("\nMovimento do Cavalo:\n");
+
+    for (i = 0; i < casasCavaloVertical; i++)
+    {
+        printf("Baixo\n");
+
+        contador = 0;
+        while (contador < casasCavaloHorizontal && i == casasCavaloVertical - 1)
+        {
+            printf("Esquerda\n");
+            contador++;
+        }
+    }
 
     return 0;
 }
